98e44d8c989627cd7222f6260c33d6f9394ecb7e
Fasakin Henry <fasakinhenry@gmail.com>
2024-05-10 12:39:58 +0100

    style: fix responsiveness for the tab screens

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
-
+let hamburger_menu = document.querySelector(".hamburger-menu");
+let big_wrapper = document.querySelector(".big-wrapper");
+hamburger_menu.addEventListener("click", ()=> {
+    big_wrapper.classList.toggle("active");
+})
