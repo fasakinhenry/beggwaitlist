8b45a8e53d962016c4877c72a1bbd107785ae815
Fasakin Henry <fasakinhenry@gmail.com>
2024-05-12 21:06:47 +0100

    feat (app.js): add backend integration for the waitlist

--- app.js
+++ app.js
@@ -1,5 +1,21 @@
 let hamburger_menu = document.querySelector(".hamburger-menu");
+let email = document.querySelector("#email");
 let big_wrapper = document.querySelector(".big-wrapper");
 hamburger_menu.addEventListener("click", ()=> {
     big_wrapper.classList.toggle("active");
-})
+});
+
+async function joinWaitlist() {
+    try {
+        const response = await fetch("https://begg-dev-backend.onrender.com/waitlist/wait", {
+            method: "POST",
+            body: {
+                "email": "alexindevs@gmail.com"
+            }
+        });
+        const waitlist_response = await response.json();
+        console.log(waitlist_response);
+    } catch (error) {
+        console.log('Error ', error);
+    }
+}
